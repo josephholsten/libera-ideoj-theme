9a34e77917ff0d455e8c0b303b75581f7b6137fe
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2009-01-21 18:33:08 -0600

    removed 'RSS' from atom feed title, removed sitename from page titles except on the index

--- trunk/header.php
+++ trunk/header.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
 <head profile="http://gmpg.org/xfn/11">
-	<title><?php if ( is_404() ) : ?><?php _e('Page not found', 'simplr') ?> &lt; <?php bloginfo('name') ?><?php elseif ( is_home() ) : ?><?php bloginfo('name') ?> &gt; <?php bloginfo('description') ?><?php elseif ( is_category() ) : ?><?php echo single_cat_title(); ?> &lt; <?php bloginfo('name') ?><?php elseif ( is_date() ) : ?><?php _e('Blog archives', 'simplr') ?> &lt; <?php bloginfo('name') ?><?php elseif ( is_search() ) : ?><?php _e('Search results', 'simplr') ?> &lt; <?php bloginfo('name') ?><?php else : ?><?php the_title() ?> &lt; <?php bloginfo('name') ?><?php endif ?></title>
+	<title><?php if ( is_404() ) : ?><?php _e('Page not found', 'simplr') ?><?php elseif ( is_home() ) : ?><?php bloginfo('name') ?><?php elseif ( is_category() ) : ?><?php echo single_cat_title(); ?><?php elseif ( is_date() ) : ?><?php _e('Blog archives', 'simplr') ?><?php elseif ( is_search() ) : ?><?php _e('Search results', 'simplr') ?><?php else : ?><?php the_title() ?><?php endif ?></title>
 	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
 	<link rel="stylesheet" type="text/css" media="screen,projection" href="<?php bloginfo('stylesheet_url'); ?>" title="Simplr" />
 	<link rel="stylesheet" type="text/css" media="print" href="<?php bloginfo('template_directory'); ?>/print.css" />
-	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php bloginfo('name') ?> RSS feed" /> 
+	<link rel="alternate" type="application/atom+xml" href="<?php bloginfo('atom_url') ?>" title="<?php bloginfo('name') ?> feed" /> 
 	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
 
 <?php wp_head() // Do not remove; helps plugins work ?>
